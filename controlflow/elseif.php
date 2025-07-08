b982a73476e6c67ed89058b8b3a7dc8e064d7328
Md Parvez Ahamed <devparvez23@gmail.com>
2025-07-08 18:17:42 +0600

    php control flow complete

---
+++ controlflow/elseif.php
@@ -0,0 +1,27 @@
+<?php 
+
+$score = 40;
+
+if($score >= 80){
+    echo "A+\n";
+}
+elseif( $score >= 70){
+    echo "a\n";
+}
+elseif( $score >= 60){
+    echo "-a\n";
+}
+elseif( $score >= 50){
+    echo "B\n";
+}
+elseif( $score >= 40){
+    echo "C\n";
+}
+elseif( $score >= 33){
+    echo "D\n";
+}else{
+    echo "Fail\n";
+}
+
+
+?>
