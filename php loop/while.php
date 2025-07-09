9c39367e1e7de835a043bdb00306df9f987cbc15
Md Parvez Ahamed <devparvez23@gmail.com>
2025-07-09 16:52:03 +0600

    PHP Loop Complete

---
+++ php loop/while.php
@@ -0,0 +1,34 @@
+<?php 
+
+// $i = 1;
+// while( $i < 5 ){
+//     echo "$i\n";
+//     $i = $i++;
+// }
+
+// for( $i = 1; $i <= 5 ; $i++ ){
+//     echo "$i\n";
+// }
+// for( $i = 5; $i >= 1 ; $i-- ){
+//     echo "$i\n";
+// }
+
+
+// $correctPassword = "123456";
+// $userInput = "";
+
+// while ($userInput !== $correctPassword){
+//     $userInput = readline('Plase give me a correct password');
+
+// }
+
+
+$i = 1;
+
+do {
+    echo "Login Attemp $i<br>";
+    $i++;
+} while ($i <= 3);
+
+
+?>
